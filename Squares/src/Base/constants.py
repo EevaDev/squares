17798546e8b72e5abd886fd0c43ef92a22b29f2c
Davide Trentin <3ntolo@gmail.com>
2013-09-23 22:12:13 -0400

    Display table, select squares

--- Squares/src/Base/constants.py
+++ Squares/src/Base/constants.py
@@ -8,6 +8,22 @@ size = (width, height) = (320, 240)
 BLACK = (0, 0, 0)
 WHITE = (255,255,255)
 
+square_colors = {
+    "magenta" : (158,14,64),
+    "blue"    : (37,191,218),
+    "green"   : (159,232,85),
+    "red"     : (232,19,40),
+    "orange"  : (242,217,51)
+}
+
 STATE_MENU = 0
 STATE_PLAY = 1
 STATE_EXIT = 2
+
+# Table and squares parameters
+TAB_W = 6
+TAB_H = 6
+
+S_SIZE = 10
+S_DETECT_SIZE = 22
+S_DISTANCE = 30
--- Squares/src/Base/match.py
+++ Squares/src/Base/match.py
@@ -5,11 +5,42 @@ Created on 2013-09-21
 '''
 import utils
 import pygame
+import random
 from constants import *
 
 class Square(object):
     def __init__(self, row, col):
-        pass
+        self.color = square_colors[random.choice(square_colors.keys())]
+        self.x = (col+1)*S_DISTANCE
+        self.y = (row+1)*S_DISTANCE
+        self.width = S_SIZE
+        self.rect = pygame.Rect(self.x,self.y,self.width,self.width)
+        self.detect_rect = pygame.Rect(self.x-(S_DETECT_SIZE-S_SIZE)/2, 
+                                       self.y-(S_DETECT_SIZE-S_SIZE)/2,
+                                       S_DETECT_SIZE, S_DETECT_SIZE)
+        self.selected = False
+    
+    def get_color(self):
+        return self.color
+
+    def isSelected(self):
+        return self.selected
+    
+    def select(self):
+        self.selected = True
+    
+    def deselect(self):
+        self.selected = False
+            
+    def isOver(self, pos):
+        return self.detect_rect.collidepoint(pos)
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+        if self.selected:
+            pygame.draw.rect(screen, self.color, self.detect_rect)
+        else:
+            pygame.draw.rect(screen, self.color, self.detect_rect, 1)
 
 class Match(object):
     '''
@@ -23,19 +54,62 @@ class Match(object):
         '''
         self.speed = [5,5]
         self.ball, self.ballrect = utils.load_image("ball.gif")
+        self.table = []
+        for r in range(TAB_H):
+            self.table.append([])
+            for c in range(TAB_W):
+                self.table[r].append(Square(r,c))
         self.screen = screen
+        self.button_down = False
+        self.current_color = (0,0,0)
+        self.chain = []
         
     def handle_event(self, ev):
-        pass
+        if ev.type == pygame.MOUSEBUTTONDOWN and ev.button == 1:
+            self.button_down = True
+            for row in range(TAB_H):
+                for col in range(TAB_W):
+                    square = self.table[row][col]
+                    if square.isOver(pygame.mouse.get_pos()):
+                        # Start a new chain
+                        self.current_color = square.get_color() 
+                        square.select()
+                        self.chain.append((row,col))
+        elif ev.type == pygame.MOUSEBUTTONUP and ev.button == 1:
+            self.button_down = False
+            self.current_color = (0,0,0)
+            if len(self.chain):
+                print self.chain
+            if len(self.chain) >= 2:
+                for (row, col) in self.chain:
+                    del(self.table[row][col])
+            ## TODO: Shift everything over the deleted ones in their places
+            
+            elif len(self.chain) == 1:
+                self.table[self.chain[0][0]][self.chain[0][1]].deselect()
+            self.chain = []
     
     def update(self):
-        self.ballrect = self.ballrect.move(self.speed)
-        if self.ballrect.left < 0 or self.ballrect.right > width:
-            self.speed[0] = -self.speed[0]
-        if self.ballrect.top < 0 or self.ballrect.bottom > height:
-            self.speed[1] = -self.speed[1]
+        if self.button_down and len(self.chain) > 0:
+            # The next square must be or in the same row or in the same col, 
+            # next to the last square of the chain
+            mouse_pos = pygame.mouse.get_pos()
+            cur_row = self.chain[-1][0]
+            cur_col = self.chain[-1][1]
+            for (new_row,new_col) in [(cur_row-1,cur_col),(cur_row+1,cur_col),(cur_row,cur_col-1),(cur_row,cur_col+1)]:
+                if new_row >= 0 and new_row < TAB_H and new_col >= 0 and new_col < TAB_W:
+                    square = self.table[new_row][new_col]
+                    if square.isOver(mouse_pos) and not square.isSelected() and square.get_color()==self.current_color:
+                        square.select()
+                        self.chain.append((new_row,new_col))
+                    if len(self.chain) >= 2:
+                        if square.isOver(mouse_pos) and new_row == self.chain[-2][0] and new_col == self.chain[-2][1]:
+                            self.table[cur_row][cur_col].deselect()
+                            self.chain.pop(-1)
             
     def draw(self):
         self.screen.fill(WHITE)
-        self.screen.blit(self.ball, self.ballrect)
+        for row in range(TAB_H):
+            for square in self.table[row]:
+                square.draw(self.screen)
         pygame.display.flip()
